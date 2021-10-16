92f407ad999e0cc5685f992d15f350c4b3793754
Chris Yoon <chy.yoon@gmail.com>
2021-10-15 22:02:16 -0700

    added consumer context to ap'

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,13 +1,36 @@
 import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Buttons extends React.Component {
-  static contextType = LanguageContext;
-
   render() {
-    const text = this.context === "english" ? "Submit" : "Voorleggen";
-    return <button className='ui button primary'>{text}</button>;
+    return (
+      <ColorContext.Consumer>
+        {(color) => (
+          <button className={`ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {(value) => (value === "english" ? "Submit" : "Voorleggen")}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
+    );
   }
 }
 
 export default Buttons;
+
+//THIs IS HOW YOU GET DATA WITH CONTEXT.TYPE
+// import React from "react";
+// import LanguageContext from "../contexts/LanguageContext";
+
+// class Buttons extends React.Component {
+//   static contextType = LanguageContext;
+
+//   render() {
+//     const text = this.context === "english" ? "Submit" : "Voorleggen";
+//     return <button className='ui button primary'>{text}</button>;
+//   }
+// }
+
+// export default Buttons;
