76584abc0661b0161340b4016394d8f389aa7cd2
Chris Yoon <chy.yoon@gmail.com>
2021-10-15 20:44:03 -0700

    added context props and default values

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange("dutch")}
           />
         </div>
-        {this.state.language}
+
         <UserCreate />
       </div>
     );
