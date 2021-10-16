2f1e98feb3d5e8193e57e9137d968246327ce379
Chris Yoon <chy.yoon@gmail.com>
2021-10-15 20:19:23 -0700

    created button, Field UserCreate comopnents

---
+++ src/components/Buttons.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Buttons extends React.Component {
+  render() {
+    return <button className='ui button primary'>Submit</button>;
+  }
+}
+
+export default Buttons;
---
+++ src/components/UserCreate.js
@@ -0,0 +1,13 @@
+import Field from "./Field";
+import Buttons from "./Buttons";
+import React from "react";
+
+const UserCreate = () => {
+  return (
+    <div className='ui form'>
+      <Field />
+      <Buttons />
+    </div>
+  );
+};
+export default UserCreate;
